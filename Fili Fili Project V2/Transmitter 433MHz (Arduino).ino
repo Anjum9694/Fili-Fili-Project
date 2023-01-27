d56e36792896613a78fc5d624f3fd5413d226d18
Anjum9694 <31078090+Anjum9694@users.noreply.github.com>
2023-01-27 13:43:16 +0500

    Update Transmitter 433MHz (Arduino).ino

--- Fili Fili Project V2/Transmitter 433MHz (Arduino).ino
+++ Fili Fili Project V2/Transmitter 433MHz (Arduino).ino
@@ -2,18 +2,17 @@
 // This code allows wireless radio transmitting with Arduino UNO
 // https://github.com/Anjum9694
 
-#include <RH_ASK.h> // Connect 433MHz transmitter to pin 12
-#include <SPI.h> // Not actually used but needed to compile
+#include <RCSwitch.h>
 
-const int buttonPin1 = 2; // Button 1 is connected to digital pin 2
-const int buttonPin2 = 3; // Button 2 is connected to digital pin 3
-const int buttonPin3 = 4; // Button 3 is connected to digital pin 4
+const int buttonPin1 = 2; // Blue Button Pin
+const int buttonPin2 = 3; // Green Button Pin
+const int buttonPin3 = 4; // Red Button Pin
 
 unsigned long code1 = 0xFFFFF1; // Code for Blue Button
 unsigned long code2 = 0xFFFFF2; // Code for Green Button
 unsigned long code3 = 0xFFFFF3; // Code for Red Button
 
-RH_ASK driver;
+RCSwitch transmitter = RCSwitch();
 
 void setup()
 {
@@ -21,27 +20,23 @@ void setup()
   pinMode(buttonPin1, INPUT); // Set button 1 as an input with a pull-up resistor
   pinMode(buttonPin2, INPUT); // Set button 2 as an input with a pull-up resistor
   pinMode(buttonPin3, INPUT); // Set button 3 as an input with a pull-up resistor
-  if (!driver.init())
-      Serial.println("RF Transmitter driver failed to initialize!");
+  transmitter.enableTransmit(12); // Connect 433MHz transmitter to pin 12
 }
 
 void loop()
 {
   if (digitalRead(buttonPin1) == HIGH) { // If Blue Button is pressed
-      driver.send((uint8_t*)&code1, sizeof(code1));
-      driver.waitPacketSent();
+      transmitter.send(code1, 24);
       Serial.println("Blue button has been pressed.");
       delay(100); // Delay to debounce the button press
   }
   if (digitalRead(buttonPin2) == HIGH) { // If Green Button is pressed
-      driver.send((uint8_t*)&code2, sizeof(code2));
-      driver.waitPacketSent();
+      transmitter.send(code2, 24);
       Serial.println("Green button has been pressed.");
       delay(100); // Delay to debounce the button press
   }
   if (digitalRead(buttonPin3) == HIGH) { // If Red Button is pressed
-      driver.send((uint8_t*)&code3, sizeof(code3));
-      driver.waitPacketSent();
+      transmitter.send(code3, 24);
       Serial.println("Red button has been pressed.");
       delay(100); // Delay to debounce the button press
   }
